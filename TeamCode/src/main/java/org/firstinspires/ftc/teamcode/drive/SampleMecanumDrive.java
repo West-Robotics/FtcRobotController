0e59725dcb564d55378a12e3602cce5780351f25
consla <conslaaa@proton.me>
2023-09-11 00:14:51 -0800

    refactor: update Roadrunner to use new robot hardware

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
@@ -28,6 +28,7 @@ import com.qualcomm.robotcore.hardware.VoltageSensor;
 import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigurationType;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.teamcode.seventh.robot.hardware.Hardware;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequenceBuilder;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequenceRunner;
@@ -54,6 +55,8 @@ import static org.firstinspires.ftc.teamcode.drive.DriveConstants.kV;
  */
 @Config
 public class SampleMecanumDrive extends MecanumDrive {
+    private Hardware hardware = Hardware.getInstance();
+
     public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(0, 0, 0);
     public static PIDCoefficients HEADING_PID = new PIDCoefficients(0, 0, 0);
 
@@ -89,20 +92,21 @@ public class SampleMecanumDrive extends MecanumDrive {
 
         batteryVoltageSensor = hardwareMap.voltageSensor.iterator().next();
 
+        // TODO: make this one hub
         for (LynxModule module : hardwareMap.getAll(LynxModule.class)) {
             module.setBulkCachingMode(LynxModule.BulkCachingMode.AUTO);
         }
 
-        // // TODO: adjust the names of the following hardware devices to match your configuration
-        // imu = hardwareMap.get(IMU.class, "imu");
-        // IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
-        //         DriveConstants.LOGO_FACING_DIR, DriveConstants.USB_FACING_DIR));
-        // imu.initialize(parameters);
+        // TODO: adjust the names of the following hardware devices to match your configuration
+//         imu = hardwareMap.get(IMU.class, "imu");
+//         IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+//                 DriveConstants.LOGO_FACING_DIR, DriveConstants.USB_FACING_DIR));
+//         imu.initialize(parameters);
 
-        leftFront = hardwareMap.get(DcMotorEx.class, "leftFront");
-        leftRear = hardwareMap.get(DcMotorEx.class, "leftRear");
-        rightRear = hardwareMap.get(DcMotorEx.class, "rightRear");
-        rightFront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        leftFront = hardware.leftFront;
+        leftRear = hardware.leftRear;
+        rightRear = hardware.rightRear;
+        rightFront = hardware.rightFront;
 
         motors = Arrays.asList(leftFront, leftRear, rightRear, rightFront);
 
@@ -129,7 +133,7 @@ public class SampleMecanumDrive extends MecanumDrive {
 
         // TODO: if desired, use setLocalizer() to change the localization method
         // setLocalizer(new StandardTrackingWheelLocalizer(hardwareMap, lastTrackingEncPositions, lastTrackingEncVels));
-        setLocalizer(new StandardTrackingWheelLocalizer(hardwareMap, lastTrackingEncPositions, lastTrackingEncVels));
+        setLocalizer(new TwoWheelTrackingLocalizer(hardwareMap, this));
 
         trajectorySequenceRunner = new TrajectorySequenceRunner(
                 follower, HEADING_PID, batteryVoltageSensor,
@@ -291,12 +295,12 @@ public class SampleMecanumDrive extends MecanumDrive {
 
     @Override
     public double getRawExternalHeading() {
-        return 0.0;
+        return hardware.getAngle();
     }
 
     @Override
     public Double getExternalHeadingVelocity() {
-        return 0.0;
+        return hardware.getAngularVelocity();
     }
 
     public static TrajectoryVelocityConstraint getVelocityConstraint(double maxVel, double maxAngularVel, double trackWidth) {
