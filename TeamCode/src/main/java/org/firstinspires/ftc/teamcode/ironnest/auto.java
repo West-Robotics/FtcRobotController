b6747df7deec02e08606f303695b72b22453446d
Aiden Gonka <akminime@yahoo.com>
2023-11-17 14:10:06 -0900

    Updated Outline

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ironnest/auto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ironnest/auto.java
@@ -7,7 +7,7 @@ import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.Controller;
-import org.firstinspires.ftc.teamcode.technomancers.TechnomancersDrive;
+import org.firstinspires.ftc.teamcode.ironnest.IronNestDrive;
 
 import java.util.concurrent.TimeUnit;
 
@@ -17,17 +17,6 @@ public class auto extends LinearOpMode {
     //So whoever looks at this code, I have no idea if it will work I just copied the code from Technomancers
 
     @Override public void runOpMode() throws InterruptedException {
-
-        IronNestDrive drive = new IronNestDrive(this, hardwareMap);
-
-        waitForStart();
-        ElapsedTime e = new ElapsedTime();
-        e.reset();
-        e.startTime();
-        while(e.time(TimeUnit.SECONDS) < 1.5) {
-            drive.drive(0,0.3,0);
-        }
-        e.reset();
-        //drive.move(0.4, 60, 0);
+        
     }
 }
