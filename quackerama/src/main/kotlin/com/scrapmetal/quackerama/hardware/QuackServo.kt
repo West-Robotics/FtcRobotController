2fb7c8d578d75eeb6c9aa63c64a6f080de78771f
consla <conslaaa@proton.me>
2023-11-24 23:49:18 -0900

    feat(quack): overload setPosition() to take a lambda

--- quackerama/src/main/kotlin/com/scrapmetal/quackerama/hardware/QuackServo.kt
+++ quackerama/src/main/kotlin/com/scrapmetal/quackerama/hardware/QuackServo.kt
@@ -11,8 +11,16 @@ import kotlin.math.abs
  *
  * @param pwm servo model, used to determine pwm range
  * @param thresh minimum change in commanded position to necessitate a hardware write
+ * @param usFrame framing rate in microseconds
  */
-class QuackServo(hardwareMap: HardwareMap, name: String, pwm: ModelPWM, private var thresh: Double = 0.001) {
+// TODO: add direction and initial position as part of the constructor to ensure that they are
+//   explicitly stated
+class QuackServo(hardwareMap: HardwareMap, name: String, pwm: ModelPWM, private var thresh: Double = 0.001, usFrame: Double = 5000.0) {
+    /**
+     * PWM ranges for various servo models
+     *
+     * Axons are limited from 510-2490 to prevent accidental wraparound (may be adjusted) in the future
+     */
     enum class ModelPWM(val min: Double, val max: Double) {
         AXON_MAX(510.0, 2490.0), AXON_MINI(510.0, 2490.0), AXON_MICRO(510.0, 2490.0),
         GOBILDA_TORQUE(500.0, 2500.0), GOBILDA_SPEED(500.0, 2500.0), GOBILDA_SUPER(500.0, 2500.0),
@@ -23,7 +31,7 @@ class QuackServo(hardwareMap: HardwareMap, name: String, pwm: ModelPWM, private 
     private var lastPosition = 0.0
 
     init {
-        servo.pwmRange = PwmControl.PwmRange(pwm.min, pwm.max)
+        servo.pwmRange = PwmControl.PwmRange(pwm.min, pwm.max, usFrame)
     }
 
     fun setDirection(direction: Direction) {
@@ -41,5 +49,17 @@ class QuackServo(hardwareMap: HardwareMap, name: String, pwm: ModelPWM, private 
         }
     }
 
+    /**
+     * @param convertToPosition convert position argument to servo's 0-1 range
+     */
+    fun setPosition(position: Double, convertToPosition: (Double) -> Double) {
+    	val convertedPosition = convertToPosition(position)
+        if (abs(convertedPosition - lastPosition) > thresh) {
+            servo.position = convertedPosition
+            lastPosition = convertedPosition
+        }
+    }
+
     fun getCommandedPosition() = servo.position
+
 }
