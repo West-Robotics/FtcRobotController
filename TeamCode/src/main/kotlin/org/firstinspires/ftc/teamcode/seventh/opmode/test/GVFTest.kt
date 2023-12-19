fec8a48132d9bed181bd15bc56d852808f1fd073
consla <conslaaa@proton.me>
2023-12-18 21:34:38 -0900

    feat(gvf): add testing paths

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/seventh/opmode/test/GVFTest.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/seventh/opmode/test/GVFTest.kt
@@ -29,10 +29,31 @@ class GVFTest : LinearOpMode() {
         val path: Path = path {
             hermite {
                 label("test")
-                start { pos(0.0, 0.0); ang(0.0); v(30.0) }
-                end { pos(25.0, 25.0); ang(0.0); v(30.0) }
+                start { pos(0.0, 0.0); ang(0.0); v(40.0) }
+                end { pos(20.0, 20.0); ang(0.0); v(40.0) }
             }
         }
+        // val path: Path = path {
+        //     hermite {
+        //         label("test")
+        //         start { pos(0.0, 0.0); ang(0.0); v(1.0) }
+        //         end { pos(100.0, 00.0); ang(0.0); v(1.0) }
+        //     }
+        // }
+        // val path: Path = path {
+        //     hermite {
+        //         label("test")
+        //         start { pos(0.0, 0.0); ang(0.0); v(180.0) }
+        //         end { pos(00.0, 80.0); ang(180.0); v(180.0) }
+        //     }
+        // }
+        // val path: Path = path {
+        //     hermite {
+        //         label("test")
+        //         start { pos(0.0, 0.0); ang(0.0); v(500.0) }
+        //         end { pos(60.0, 80.0); ang(0.0); v(500.0) }
+        //     }
+        // }
         val gvf = GVF(path, 0.2)
         Robot.hardwareMap = hardwareMap
         val allHubs = hardwareMap.getAll(LynxModule::class.java)
@@ -70,12 +91,12 @@ class GVFTest : LinearOpMode() {
             val rrPose = RRPose2d(m_d.u, m_d.v, 0.0)
             // val rrPose = RRPose2d(primary.leftY, -primary.leftX, -primary.rightX)
             val heading = drive.poseEstimate.heading
-            val distToEnd = path.paths[0].endPose.position.distanceTo(Vector2d(drive.poseEstimate.x, drive.poseEstimate.y))/3
+            val distToEnd = path.paths[0].endPose.position.distanceTo(Vector2d(drive.poseEstimate.x, drive.poseEstimate.y))/24
             val multiplier = if (distToEnd > 1.0) 1.0 else distToEnd
-            val correctedRRPose = RRPose2d((rrPose.y*sin(heading) + rrPose.x*cos(heading))*multiplier*0.4,
-                    (rrPose.y*cos(heading) - rrPose.x*sin(heading))*multiplier*0.7,
+            val correctedRRPose = RRPose2d((rrPose.y*sin(heading) + rrPose.x*cos(heading))*multiplier*0.8,
+                    (rrPose.y*cos(heading) - rrPose.x*sin(heading))*multiplier*1.0,
                     0.0)
-            // drive.setWeightedDrivePower(correctedRRPose)
+            drive.setWeightedDrivePower(correctedRRPose)
 
             telemetry.addData("hz", 1000 / dt)
             telemetry.addData("m_d angle", Math.toDegrees(m_d.polarAngle))
