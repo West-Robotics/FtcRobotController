ec43977c89a7c6985c7b501f0ad75882ee0d2427
consla <conslaaa@proton.me>
2024-01-22 14:18:40 -0900

    feat(vision): atags?

--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/seventh/opmode/test/CameraTest.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/seventh/opmode/test/CameraTest.kt
@@ -4,24 +4,23 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode
 import org.firstinspires.ftc.teamcode.seventh.robot.hardware.Globals
 import org.firstinspires.ftc.teamcode.seventh.robot.hardware.Hardware
+import org.firstinspires.ftc.teamcode.seventh.robot.vision.Vision
 
 @Autonomous(name = "CameraTest")
 class CameraTest : LinearOpMode() {
     override fun runOpMode() {
         Globals.AUTO = true
-        val hardware = Hardware.getInstance(hardwareMap)
-        var x = 0.0
-        var y = 0.0
-        var avgX = 0.0
-        var avgY = 0.0
-        var atagCount = 0
+        val vision = Vision(hardwareMap)
+        // vision.disableProp()
+        vision.enableAtag()
 
         while (opModeInInit()) {
-            for (det in hardware.aprilTag.detections) {
-                atagCount++
-            }
-            telemetry.addData("prop position", hardware.propProcessor.getPosition())
+            // telemetry.addData("fps", vision.getFps())
+            // telemetry.addData("count", vision.getDets()?.size)
+            // telemetry.addData("average dist", vision.getDistance())
+            // vision.getDets()?.forEach { telemetry.addData("det ${it.id}", it.ftcPose.y) }
             telemetry.update()
         }
+        return
     }
 }
--- TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/seventh/robot/vision/Vision.kt
+++ TeamCode/src/main/kotlin/org/firstinspires/ftc/teamcode/seventh/robot/vision/Vision.kt
@@ -5,11 +5,13 @@ import com.qualcomm.robotcore.hardware.HardwareMap
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName
 import org.firstinspires.ftc.teamcode.seventh.robot.subsystem.GetPropPositionPipeline
 import org.firstinspires.ftc.vision.VisionPortal
+import org.firstinspires.ftc.vision.apriltag.AprilTagDetection
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor
 import org.openftc.easyopencv.OpenCvCamera
 import org.openftc.easyopencv.OpenCvCameraFactory
 import org.openftc.easyopencv.OpenCvCameraRotation
 
+// TODO: conflicting camera rez and closed devices?
 class Vision(hardwareMap: HardwareMap) {
     // === eocv ===
     val cameraMonitorViewId: Int = hardwareMap.appContext.resources.getIdentifier(
@@ -28,20 +30,20 @@ class Vision(hardwareMap: HardwareMap) {
     val visionPortal = VisionPortal.Builder()
             .setCamera(hardwareMap.get(WebcamName::class.java, "camera"))
             .addProcessor(atagProcessor)
-            .setCameraResolution(Size(640, 480))
+            .setCameraResolution(Size(1280, 720))
             .setStreamFormat(VisionPortal.StreamFormat.MJPEG)
             .enableLiveView(false)
             .setAutoStopLiveView(true)
             .build()
 
     init {
-        camera.openCameraDeviceAsync( object : OpenCvCamera.AsyncCameraOpenListener {
-            override fun onOpened() {
-                camera.startStreaming(1280, 720, OpenCvCameraRotation.UPSIDE_DOWN);
-            }
+        // camera.openCameraDeviceAsync( object : OpenCvCamera.AsyncCameraOpenListener {
+        //     override fun onOpened() {
+        //         camera.startStreaming(1280, 720, OpenCvCameraRotation.UPSIDE_DOWN);
+        //     }
 
-            override fun onError(errorCode: Int) {}
-        })
+        //     override fun onError(errorCode: Int) {}
+        // })
         visionPortal.setProcessorEnabled(atagProcessor, false)
     }
 
@@ -49,18 +51,18 @@ class Vision(hardwareMap: HardwareMap) {
         return propPosition.getPosition()
     }
 
-    fun getDistance(): Double {
-        return atagProcessor.freshDetections?.fold(0.0)
-            { mean, det -> mean + det.ftcPose.y/atagProcessor.freshDetections.size } ?: 0.0
-    }
+    // fun getDistance(): Double {
+    //     return atagProcessor?.freshDetections?.fold(0.0)
+    //         { mean, det -> mean + det.ftcPose.y/atagProcessor.freshDetections?.size } ?: 0.0
+    // }
 
     fun enableProp() {
         camera.setPipeline(propPosition)
     }
 
-    fun disableProp() {
-        camera.closeCameraDevice()
-    }
+    // fun disableProp() {
+    //     camera.closeCameraDevice()
+    // }
 
     fun enableAtag() {
         visionPortal.setProcessorEnabled(atagProcessor, true)
@@ -69,6 +71,9 @@ class Vision(hardwareMap: HardwareMap) {
     fun disableAtag() {
         visionPortal.setProcessorEnabled(atagProcessor, false)
     }
+
+    fun getFps() = visionPortal.fps
+    fun getDets() = atagProcessor?.freshDetections
 
     // TODO: get fps, stream format, toggle atag, optimize gain/exposure/focus
     // disable prop
