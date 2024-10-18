e3bd291009d3c5a75942238190758d0d4e5ec05b
jcoopdog <cooper.essary@gmail.com>
2024-10-17 16:48:58 -0800

    more robert updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robertMkII/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robertMkII/DriveTrain.java
@@ -18,7 +18,7 @@ public class DriveTrain {
     private DcMotor armRotater;
     private CRServo leftHandIntake;
     private CRServo rightHandIntake;
-    private Servo handRotation;
+    private CRServo handRotater;
 
     public DriveTrain(HardwareMap hardwareMap) {
         leftFront = hardwareMap.get(DcMotor.class, "leftFront");
@@ -31,6 +31,7 @@ public class DriveTrain {
 
         leftHandIntake = hardwareMap.get(CRServo.class, "leftHandIntake");
         rightHandIntake = hardwareMap.get(CRServo.class, "rightHandIntake");
+        handRotater = hardwareMap.get(CRServo.class, "handRotater");
 
         rightBack.setDirection(DcMotorSimple.Direction.REVERSE);
         rightFront.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -70,17 +71,22 @@ public class DriveTrain {
         // diameter of pulley gear is 120mm
         // slide extension amount is 107 cm
         // full rotations until extended is 59
-
+        // worm gear is 28:1 or 14:0.5
         double extenderRevCount = armExtender.getCurrentPosition() / 537.7;
-        if ((extenderRevCount < 89 && extendSpeed > 0) || (extenderRevCount > 0 && extendSpeed < 0)) {
+        double rotationCount = armRotater.getCurrentPosition() / 537.7;
+        if ((extenderRevCount < 89 && extendSpeed > 0) || (extenderRevCount > 2 && extendSpeed < 0)) {
             armExtender.setPower(extendSpeed);
-        }
-        armRotater.setPower(rotateSpeed);
+        } else { armExtender.setPower(0); }
+        if ((rotationCount < 13.5 && rotateSpeed > 0) || (rotationCount > 2 && rotateSpeed < 0)) {
+            armRotater.setPower(rotateSpeed);
+        } else { armRotater.setPower(0); }
 
     }
 
     public void moveHand(double handRotateSpeed, double handIntakeSpeed) {
         leftHandIntake.setPower(handIntakeSpeed);
         rightHandIntake.setPower(handIntakeSpeed);
+
+        handRotater.setPower(handRotateSpeed);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robertMkII/RobertMkIITeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robertMkII/RobertMkIITeleOp.java
@@ -1,6 +1,11 @@
 package org.firstinspires.ftc.teamcode.robertMkII;
-
-
+/*
+TODO:
+- make hand pos preset
+- rebind the other things
+- implement the old controller new controller thing? (or use bools to define what pos or enum)
+- buy bricks
+*/
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -22,7 +27,7 @@ public class RobertMkIITeleOp extends OpMode {
         } else {
             drivetrain.tankDrive(-gamepad1.left_stick_y, gamepad1.left_stick_x, gamepad1.right_stick_x);
         }
-        drivetrain.manipulateArm(-gamepad2.left_stick_y, -gamepad2.right_stick_y);
-        drivetrain.moveHand(0,gamepad2.left_trigger-gamepad2.right_trigger);
+        drivetrain.manipulateArm(-gamepad2.left_stick_y, gamepad2.left_stick_x);
+        drivetrain.moveHand(-gamepad1.right_stick_y,gamepad2.left_trigger-gamepad2.right_trigger);
     }
 }
