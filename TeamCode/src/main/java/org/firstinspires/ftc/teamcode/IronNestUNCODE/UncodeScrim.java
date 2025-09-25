03157a1a8429bad636597154a69661754cbff19f
Ryan Guy Edimo <144562493+Westhighroboticryanedimo@users.noreply.github.com>
2025-09-24 16:08:40 -0800

    UNCODE TELEOP
    kj/l.a ihlagrshjadgslhu

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IronNestUNCODE/UncodeScrim.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IronNestUNCODE/UncodeScrim.java
@@ -1,18 +1,13 @@
-package org.firstinspires.ftc.teamcode.intoTheDeep_ironNest;
-import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_USING_ENCODER;
-import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.RUN_WITHOUT_ENCODER;
-import static com.qualcomm.robotcore.hardware.DcMotor.RunMode.STOP_AND_RESET_ENCODER;
+package org.firstinspires.ftc.teamcode.IronNestUNCODE;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.Controller;
 
-@TeleOp(name="shdfijoagrskhoegsij[o")
-public class Basic_teleOp extends LinearOpMode{
+@TeleOp(name="Scrimtele")
+public class UncodeScrim extends LinearOpMode{
 
     private DcMotor FL;
     private DcMotor FR;
